520aca63239ab609a9df0d14559fe1e4102710d4
Chris Day <39862618+Voldeemort@users.noreply.github.com>
2022-08-23 15:19:26 +0800

    Update subinfo.js

--- module/Subinfo/subinfo.js
+++ module/Subinfo/subinfo.js
@@ -17,15 +17,7 @@ let args = getArgs();
   let total = info.total;
   let expire = args.expire || info.expire;
   let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
-
-  if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天`);
-  }
-  if (expire && expire !== "false") {
-    if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    content.push(`到期：${formatTime(expire)}`);
-  }
-
+  
   let now = new Date();
   let hour = now.getHours();
   let minutes = now.getMinutes();
