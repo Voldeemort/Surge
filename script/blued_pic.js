ecc574574e1070f871c641b6ecf2b576ad58bcc8
Colin <39862618+Voldeemort@users.noreply.github.com>
2024-07-17 06:41:41 +0800

    Update blued_pic.js

--- script/blued_pic.js
+++ script/blued_pic.js
@@ -13,12 +13,18 @@ const notificationTitle = "成功捕获密照";
 const notificationBody = "点击查看";
 const notificationURL = $request.url;
 
+// 读取已通知的URL列表
 const storedURLs = $persistentStore.read("notifiedURLs") ? JSON.parse($persistentStore.read("notifiedURLs")) : [];
 
+// 检查是否已通知过该URL
 if (!storedURLs.includes(notificationURL)) {
-    $notification.post(notificationTitle, notificationBody, { "url": notificationURL });
+    // 发送通知，并设置点击通知后打开的URL
+    $notification.post(notificationTitle, notificationBody, { "open-url": notificationURL });
+
+    // 将该URL添加到已通知列表中
     storedURLs.push(notificationURL);
     $persistentStore.write(JSON.stringify(storedURLs), "notifiedURLs");
 }
 
+// 完成脚本执行
 $done({});
