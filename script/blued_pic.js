c8c46556d7a32fef8f0f8415aaef14315e7e4a67
Colin <39862618+Voldeemort@users.noreply.github.com>
2024-07-17 06:35:27 +0800

    Update blued_pic.js

--- script/blued_pic.js
+++ script/blued_pic.js
@@ -3,7 +3,7 @@
 // Surge Script Format
 /*
 [Script]
-闪照捕获 = type=http-response, pattern=https:\/\/burn-chatfiles\.bldimg\.com\/burn_chatfiles\/users\/.*\.jpg!o.png, requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/Voldeemort/Surge/main/script/blued_pic.js
+闪照捕获 = type=http-response, pattern=https:\/\/burn-chatfiles\.bldimg\.com\/burn_chatfiles\/users\/.*\.jpg!o.png, requires-body=1, max-size=0, script-path=path_to_this_script.js
 
 [MITM]
 hostname = burn-chatfiles.bldimg.com
@@ -16,7 +16,7 @@ const notificationURL = $request.url;
 const storedURLs = $persistentStore.read("notifiedURLs") ? JSON.parse($persistentStore.read("notifiedURLs")) : [];
 
 if (!storedURLs.includes(notificationURL)) {
-    $notification.post(notificationTitle, notificationBody, { "open-url": notificationURL });
+    $notification.post(notificationTitle, notificationBody, { "url": notificationURL });
     storedURLs.push(notificationURL);
     $persistentStore.write(JSON.stringify(storedURLs), "notifiedURLs");
 }
